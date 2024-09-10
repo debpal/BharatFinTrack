ce22e91a56989832007305ca0d5c4ab41f86152a
Debasish Pal <48341250+debpal@users.noreply.github.com>
2024-09-10 21:54:39 +0300

    update package, docs, .toml and add setup.cfg

--- BharatFinTrack/nse_track.py
+++ BharatFinTrack/nse_track.py
@@ -1,29 +1,32 @@
 class NSETrack:
-    
+
     '''
     Represents characteristics of NSE finance products.
     '''
-    
-    
+
     @property
-    def indices_category(self):
-        
+    def indices_category(
+        self
+    ) -> list[str]:
+
         '''
         Returns a list categories for NSE indices.
         '''
-        
+
         output = [
-            'broad', 
-            'sectoral', 
-            'thematic', 
+            'broad',
+            'sectoral',
+            'thematic',
             'strategy'
         ]
-        
+
         return output
-    
-    
-    def get_indices_by_category(self, category):
-        
+
+    def get_indices_by_category(
+        self,
+        category: str
+    ) -> list[str]:
+
         '''
         Returns NSE indices for a specified category.
 
@@ -37,57 +40,60 @@ class NSETrack:
         list
             A list containing the names of indices for the specified category.
         '''
-        
+
         indices = {}
-        
+
         # broad index
         indices['broad'] = [
             'NIFTY 500',
             'NIFTY 50'
         ]
-        
+
         # sectoral index
         indices['sectoral'] = [
             'NIFTY IT',
             'NIFTY BANK'
         ]
-        
+
         # thematic index
         indices['thematic'] = [
             'NIFTY EV & NEW AGE AUTOMOTIVE',
             'NIFTY INDIA DEFENCE'
         ]
-        
+
         # strategy index
         indices['strategy'] = [
             'NIFTY ALPHA 50',
             'NIFTY MIDCAP150 MOMENTUM 50'
         ]
-        
+
         if category in self.indices_category:
             pass
         else:
             raise Exception(f'Invadid category: {category}')
-        
+
         return indices[category]
-    
-    
+
     @property
-    def downloadable_indices(self):
-        
+    def downloadable_indices(
+        self
+    ) -> list[str]:
+
         '''
         Returns a list of all indices names.
         '''
-        
-        output = [
-            index for category in self.indices_category for index in self.get_indices_by_category(category)
-        ]
-        
-        return output
-    
 
-    def is_downloadable_index(self, index):
-        
+        output = [
+            i for c in self.indices_category for i in self.get_indices_by_category(c)
+        ]
+
+        return output
+
+    def is_downloadable_index(
+        self,
+        index: str
+    ) -> bool:
+
         '''
         Checks whether a specified NSE index name is downloadable.
 
@@ -101,22 +107,23 @@ class NSETrack:
         bool
             True if the index name is valid, False.
         '''
-        
+
         return index in self.downloadable_indices
-    
-    
+
     @property
-    def indices_base_date(self):
-        
+    def indices_base_date(
+        self
+    ) -> dict[str, str]:
+
         '''
-        Returns a dictionary where keys are indices 
+        Returns a dictionary where keys are indices
         and values are their corresponding base dates.
         '''
-        
+
         default_date = '01-Apr-2005'
-        
+
         start_date = {}
-        
+
         start_date['01-Jan-1995'] = ['NIFTY 500']
         start_date['03-Nov-1995'] = ['NIFTY 50']
         start_date['01-Jan-1996'] = ['NIFTY IT']
@@ -128,16 +135,19 @@ class NSETrack:
         ]
 
         date_dict = {v: key for key, value in start_date.items() for v in value}
-        
-        output = dict(
-            map(lambda x: (x, date_dict.get(x, default_date)), self.downloadable_indices)
-        )
-        
+
+        output = {
+            index: date_dict.get(index, default_date)
+            for index in self.downloadable_indices
+        }
+
         return output
-    
-    
-    def get_index_base_date(self, index):
-        
+
+    def get_index_base_date(
+        self,
+        index: str
+    ) -> str:
+
         '''
         Returns the base date for a specified NSE index.
 
@@ -151,17 +161,19 @@ class NSETrack:
         str
             The base date of the index in 'DD-MMM-YYYY' format.
         '''
-        
+
         if self.is_downloadable_index(index):
             pass
         else:
             raise Exception(f'Invalid index: {index}')
-        
+
         return self.indices_base_date[index]
-    
-    
-    def get_index_base_value(self, index):
-        
+
+    def get_index_base_value(
+        self,
+        index: str
+    ) -> float:
+
         '''
         Returns the base value for a specified NSE index.
 
@@ -175,12 +187,12 @@ class NSETrack:
         float
             The base value of the index.
         '''
-        
+
         if self.is_downloadable_index(index):
             pass
         else:
             raise Exception(f'Invalid index: {index}')
-            
+
         base_value = {'NIFTY IT': 100.0}
-        
+
         return base_value.get(index, 1000.0)
