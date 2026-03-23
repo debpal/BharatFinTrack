ccb09643386f53f632daa5ecab1d60969bc373db
unknown <bestdebasish@gmail.com>
2026-03-23 12:54:28 +0530

    Added Analyzer class by shifting some methods from NSETRI class

--- BharatFinTrack/__init__.py
+++ BharatFinTrack/__init__.py
@@ -3,6 +3,7 @@ from .nse_pri import NSEPRI
 from .nse_tri import NSETRI
 from .cagr import CAGR
 from .sip import SIP
+from .analyzer import Analyzer
 
 
 __all__ = [
@@ -10,7 +11,8 @@ __all__ = [
     'NSEPRI',
     'NSETRI',
     'CAGR',
-    'SIP'
+    'SIP',
+    'Analyzer'
 ]
 
 
