c9a63074b8a8b50dac05a0f6a333753754a4aefb
unknown <bestdebasish@gmail.com>
2026-03-23 12:54:58 +0530

    Add test functions to increase code coverage

--- tests/test_nse_pri.py
+++ tests/test_nse_pri.py
@@ -17,28 +17,44 @@ def cagr():
     yield BharatFinTrack.CAGR()
 
 
-def test_equity_index_price_download_updated_value(
+@pytest.fixture(scope='class')
+def analyzer():
+
+    yield BharatFinTrack.Analyzer()
+
+
+def test_download_equity_index_close(
     nse_pri,
-    cagr
+    cagr,
+    analyzer
 ):
 
     with tempfile.TemporaryDirectory() as tmp_dir:
 
         # Pass test for downloading daily closing value
-        close_df = nse_pri.download_equity_close(
+        df = nse_pri.download_equity_close(
             csv_file=os.path.join(tmp_dir, 'PRI_daily_close.csv'),
             untracked_base_equity=True
         )
-        assert isinstance(close_df, pandas.DataFrame)
-        assert close_df.shape[1] == 7
+        assert isinstance(df, pandas.DataFrame)
+        assert df.shape[1] == 7
         assert os.path.exists(os.path.join(tmp_dir, 'PRI_daily_close.csv'))
 
+        # Pass test for sorting closing value
+        df = analyzer.sort_equity_index_close(
+            csv_file=os.path.join(tmp_dir, 'PRI_daily_close.csv'),
+            excel_file=os.path.join(tmp_dir, 'sort_closing.xlsx')
+        )
+        assert isinstance(df, pandas.DataFrame)
+        assert df.shape[1] == 6
+        assert os.path.exists(os.path.join(tmp_dir, 'sort_closing.xlsx'))
+
         # Pass test for sorting CAGR within category
-        cagr_df = cagr.sort_indices_since_launch(
+        df = cagr.sort_indices_since_launch(
             csv_file=os.path.join(tmp_dir, 'PRI_daily_close.csv'),
             excel_file=os.path.join(tmp_dir, 'sort_cagr_within_category.xlsx'),
             within_category=True
         )
-        assert isinstance(cagr_df, pandas.DataFrame)
-        assert cagr_df.shape[1] == 8
+        assert isinstance(df, pandas.DataFrame)
+        assert df.shape[1] == 8
         assert os.path.exists(os.path.join(tmp_dir, 'sort_cagr_within_category.xlsx'))
